fe78c4a19e517f5d36145233b961c787f1d4dfcb
t420babe <t420babe@gmail.com>
2020-08-16 14:15:06 -0400

    Math functions and tests

--- lib/common/math-functions.glsl
+++ lib/common/math-functions.glsl
@@ -6,12 +6,14 @@
 #endif
 
 #define CURVE(f) vec3(smoothstep(-0.75, 0.75, (f - 0.1) / fwidth(f)))
+
 #define ONE_MINUS_ABS_POW(x, e) 1.0 - pow(abs(x), e)
-#define ONE_MINUS_POW_ABS_COS(x, d, e) pow(cos( PI * x / d), e)
-#define ONE_MINUS_POW_SIN(x, d, e) pow(sin( PI * x / d), e)
-#define ONE_MINUS_POW_SIN_ALL(x, d, e, a, phi, k) pow(a * sin( PI * x / d + phi) + k, e)
-#define ONE_MINUS_POW_ABS_SIN(x, d, e) 1.0 - pow(abs(sin(PI * x / d)), e)
-#define POW_MIN_COS_MINUS_ABS(x, d, e) pow(min(cos(PI * x / d), 1.0 - abs(x)), e)
+#define ONE_MINUS_POW_COS(x, e) pow(cos( PI * x ), e)
+#define ONE_MINUS_POW_SIN(x, e) pow(sin( PI * x ), e)
+#define ONE_MINUS_POW_SIN_ALL(x, e, a, phi, k) pow(a * sin( PI * x + phi) + k, e)
+#define ONE_MINUS_POW_ABS_SIN(x, e) 1.0 - pow(abs(sin( PI * x )), e)
 #define ONE_MINUS_POW_MAX_ABS(x, e) 1.0 - pow(max(0.0, abs(x) * 2.0 - 1.0), e)
+#define POW_MIN_COS_MINUS_ABS(x, e) pow(min(cos(PI * x ), 1.0 - abs(x)), e)
+
 
 #endif
