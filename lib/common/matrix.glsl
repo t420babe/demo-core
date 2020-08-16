3ecfaa2ec6c7456653de048251afdac75c6a6dc3
t420babe <t420babe@gmail.com>
2020-08-16 17:51:01 -0400

    Black white & red cyan rainbow swirl

--- lib/common/matrix.glsl
+++ lib/common/matrix.glsl
@@ -5,5 +5,10 @@ vec2 rotate(vec2 pos, float a) {
   return mat2(cos(a), -sin(a), sin(a), cos(a)) * pos;
 }
 
+vec2 rotate(vec2 pos, float a, float offset_value) {
+  pos = mat2(cos(a), -sin(a), sin(a), cos(a)) * (pos - offset_value);
+  return pos + offset_value;
+}
+
 #endif
 
