8db1f33571275a94f323e9b9f6baede82a4afee1
t420babe <t420babe@gmail.com>
2020-08-18 00:13:06 -0400

    Audio in soft retro step

--- lib/t420babe/soft-retro-step.glsl
+++ lib/t420babe/soft-retro-step.glsl
@@ -5,13 +5,24 @@
 #include "../pixel-spirit-deck/aastep.glsl"
 #endif
 
-vec3 soft_retro_step(vec4 frag_coord, vec2 u_r) {
-  vec2 pos = (2.0 * gl_FragCoord.xy - u_r.xy) / u_r.y;
+vec3 retro_step(vec2 pos, vec2 u_r) {
   float f = aastep(pos.x, 0.5);
   float x = 0.1;
   float w = 0.5;
   float s = step(f, x + w * 0.5) - step(f, x - w * 0.5);
   vec3 color = vec3(s, pos.x, pos.y);
+  return color;
+}
+
+vec3 soft_retro_step(vec4 frag_coord, vec2 u_r, float full_ave) {
+  vec2 pos = (2.0 * frag_coord.xy - u_r.xy) / u_r.y;
+  vec3 color = vec3(1.0);
+
+  if (full_ave > 2.5) {
+    color = retro_step(pos, u_r) * full_ave;
+  } else {
+    color = retro_step(pos, u_r);
+  }
   return color;
 }
 
--- test/test-t420babe/test-soft-retro-step.frag
+++ test/test-t420babe/test-soft-retro-step.frag
@@ -5,18 +5,16 @@ precision mediump float;
 uniform vec2 u_resolution;
 uniform float u_time;
 
+uniform float full_min;
+uniform float full_ave;
+uniform float full_max;
+
 #ifndef SOFT_RETRO_STEP
-#include "lib/t420babe/soft-retro-step.glsl"
+#include "../../lib/t420babe/soft-retro-step.glsl"
 #endif
 
 void main() {
-  /* BEGIN TESTS */
-
-  // Test 0: soft_retro_step(vec4 frag_coord, vec2 u_r)
-  vec3 color = soft_retro_step(gl_FragCoord, u_resolution);
-
-  /* END TESTS */
+  vec3 color = soft_retro_step(gl_FragCoord, u_resolution, full_ave);
 
   gl_FragColor = vec4(color, 1.0);
 }
-
