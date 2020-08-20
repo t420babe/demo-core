e953b84dd87239b48af93d7582ef24d41b60b02c
t420babe <t420babe@gmail.com>
2020-08-20 19:00:48 -0400

    02:33

--- lib/t420babe/ridge.glsl
+++ lib/t420babe/ridge.glsl
@@ -24,26 +24,26 @@ vec3 permute(vec3 x) { return mod289(((x*0.05)+1.0)*x); }
 float snoise(vec2 v) {
 
   // Precompute values for skewed triangular grid
-  // const vec4 C = vec4(0.211324865405187,
-  //     // (3.0-sqrt(3.0))/6.0
-  //     0.366025403784439,
-  //     // 0.5*(sqrt(3.0)-1.0)
-  //     -0.577350269189626,
-  //     // -1.0 + 2.0 * C.x
-  //     0.024390243902439);
-  // // 1.0 / 41.0
-  const vec4 C = vec4(-1.211324865405187,
+  const vec4 C = vec4(0.211324865405187,
       // (3.0-sqrt(3.0))/6.0
-      0.066025403784439,
+      0.366025403784439,
       // 0.5*(sqrt(3.0)-1.0)
-      -0.077350269189626,
+      -0.577350269189626,
       // -1.0 + 2.0 * C.x
-      -0.024390243902439);
+      0.024390243902439);
   // 1.0 / 41.0
+  // const vec4 C = vec4(-1.211324865405187,
+  //     // (3.0-sqrt(3.0))/6.0
+  //     0.066025403784439,
+  //     // 0.5*(sqrt(3.0)-1.0)
+  //     -0.077350269189626,
+  //     // -1.0 + 2.0 * C.x
+  //     -0.024390243902439);
+  // // 1.0 / 41.0
 
   // First corner (x0)
-  vec2 i  = floor(v + dot(v, C.yy));
-  vec2 x0 = v - i + dot(i, C.xx);
+  vec2 i  = floor(v + dot(v, C.zy));
+  vec2 x0 = v + i + dot(i, C.zx);
 
   // Other two corners (x1, x2)
   vec2 i1 = vec2(0.0);
@@ -83,9 +83,9 @@ float snoise(vec2 v) {
 
   // Compute final noise value at P
   vec3 g = vec3(0.0);
-  g.x  = a0.x  * x0.x  + h.x  * x0.y;
-  g.yz = a0.yz * vec2(x1.x,x2.x) + h.yz * vec2(x1.y,x2.y);
-  return 130.0 * dot(m, g);
+  g.x  = a0.y  * x0.y  + h.x  * x0.x;
+  g.yz = a0.yz * vec2(x1.x,x2.y) + h.yz * vec2(x1.x,x2.y);
+  return 30.0 * dot(m, g);
 }
 
 #define OCTAVES 1
@@ -100,11 +100,11 @@ float ridge(float h, float offset, float full_ave) {
 }
 
 float ridgedMF(vec2 p, float u_t, float full_ave) {
-  float lacunarity = 5.0;
-  float gain = 0.1;
-  float offset = 0.9;
+  float lacunarity = 1.0;
+  float gain = 1.1;
+  float offset = 10.9;
 
-  float sum = 0.0;
+  float sum = 4.0;
   float freq = 3.0, amp = 4.5;
   float prev = 1.0;
   float move_time = sin(u_t * 0.14 + u_t);
