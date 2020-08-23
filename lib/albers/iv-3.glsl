0292ad4f5fa5db907e8602e6a617dceeb3f67b77
t420babe <t420babe@gmail.com>
2020-08-22 22:27:05 -0400

    Albers iv-3

--- lib/albers/iv-3.glsl
+++ lib/albers/iv-3.glsl
@@ -5,27 +5,7 @@
 #include "lib/common/shapes.glsl"
 #endif
 
-
-
-vec3 albers_iv_3(vec2 pos) {
-  vec3 color = vec3(0.2);
-  
-  vec3 robins_shell = vec3(0.392157, 0.427451, 0.470588);
-  vec3 green = vec3(0.501961, 0.513725, 0.337253);
-  vec3 yellow = vec3(0.772549, 0.629126, 0.270588);
-
-  color = mix(robins_shell, yellow, step(0.0, pos.y));
-
-  // (-0.375, 0.75)   (0.375, 0.75)
-  // (-0.375, 0.25)   (0.375, 0.25)
-  // (h: 0.5, w: 0.75)
-  vec2 dim = vec2(0.75, 0.5);
-  vec2 origin = vec2(0.0, 0.5);
-  float rect_upper = rectangle(pos, origin, dim);
-  color = mix(color, green, rect_upper);
-
-  int n_row = 10;
-  int n_col = 6;
+vec3 rectangle_grid(vec2 pos, vec2 origin, vec2 dim, vec3 color, vec3 grid_color, int n_row, int n_col) {
   vec2 grid_dim = vec2(dim.x / float(n_row + 1), dim.y / float(n_col + 1));
   vec2 origin_llc =  vec2(-dim.x / 2.0 + grid_dim.x / 2.0, dim.y / 2.0 + grid_dim.y / 2.0);
   float grid_ij = 0.0;
@@ -40,10 +20,47 @@ vec3 albers_iv_3(vec2 pos) {
 
         grid_ij = rectangle(pos, origin_ij, grid_dim);
 
-        color = mix(color, robins_shell, grid_ij);
+        color = mix(color, grid_color, grid_ij);
       }
     }
   }
+
+  return color;
+}
+
+
+vec3 albers_iv_3(vec2 pos) {
+  vec3 color = vec3(0.2);
+  
+  vec3 robins_shell = vec3(0.392157, 0.427451, 0.470588);
+  vec3 green = vec3(0.501961, 0.513725, 0.337253);
+  vec3 yellow = vec3(0.772549, 0.629126, 0.270588);
+
+  color = mix(yellow, robins_shell, step(0.0, pos.y));
+
+  /* Upper rectangle grid */
+  // (-0.375, 0.75)   (0.375, 0.75)
+  // (-0.375, 0.25)   (0.375, 0.25)
+  // (h: 0.5, w: 0.75)
+  vec2 dim_upper = vec2(0.75, 0.5);
+  vec2 origin_upper = vec2(0.0, 0.5);
+  int n_row = 10;
+  int n_col = 6;
+  float rect_upper = rectangle(pos, origin_upper, dim_upper);
+  color = mix(color, green, rect_upper);
+  color = rectangle_grid(pos, origin_upper, dim_upper, color, robins_shell, n_row, n_col);
+
+
+  /* Lower rectangle grid */
+  // (-0.375, -0.75)   (0.375, -0.75)
+  // (-0.375, -0.25)   (0.375, -0.25)
+  // (h: 0.5, w: 0.75)
+  vec2 dim_lower = vec2(0.75, 0.5);
+  vec2 origin_lower = vec2(0.0, -0.5);
+  float rect_lower = rectangle(pos, origin_lower, dim_lower);
+  color = mix(color, green, rect_lower);
+  color = rectangle_grid(-pos, origin_lower, dim_lower, color, yellow, n_row, n_col);
+
 
   return color;
 }
