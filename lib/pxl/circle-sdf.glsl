5df41cea251b1b3f649f556a56661accfb87ea3e
t420babe <t420babe@gmail.com>
2020-08-25 20:56:05 -0400

    20:55 green concentric pulsating circles

--- lib/pxl/circle-sdf.glsl
+++ lib/pxl/circle-sdf.glsl
@@ -32,7 +32,11 @@ float circle_0(vec2 st, float u_full_ave) {
     return length(st-.5) * 1.5;
 }
 
-float circle_1(vec2 st, float u_notch) {
+float circle_1(vec2 st, float radius) {
+    return length(st) * radius;
+}
+
+float circle_1a(vec2 st, float u_notch) {
     return length(st) * 1.0 * u_notch;
 }
 
