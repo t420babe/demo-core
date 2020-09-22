634996077f629f4692f552a412e2328943089af4
t420babe <t420babe@gmail.com>
2020-09-21 21:12:09 -0400

    lib/t420babe/couch/couch-1.glsl

--- lib/t420babe/couch/couch-1.glsl
+++ lib/t420babe/couch/couch-1.glsl
@@ -24,7 +24,7 @@ float couch1_noise (in vec2 st, peakamp audio) {
 
     return mix(a, c, u.x) +
             (c - a)* u.x * (1.0 - u.x) +
-            (d - b) * u.y * u.y;
+            (d - b) * u.y * u.x;
 }
 
 float couch1_fbm (in vec2 st, peakamp audio) {
