6bdc0ae6c1ab62f6305287683a0412f641725cd3
t420babe <t420babe@gmail.com>
2020-09-21 21:54:03 -0400

    lib/t420babe/couch/couch-1.glsl

--- lib/t420babe/couch/couch-1.glsl
+++ lib/t420babe/couch/couch-1.glsl
@@ -13,7 +13,7 @@ float couch1_random (in vec2 st) {
 float couch1_noise (in vec2 st, peakamp audio) {
     // vec2 i = log(st.xx *  0.00001);
     vec2 i = (tan(st.xx *  0.00001));
-    vec2 f = fract((st.yy * 0.001));
+    vec2 f = fract((st.yy * 0.01));
 
     // Four corners in 2D of a tile
     float a = couch1_random(i);
