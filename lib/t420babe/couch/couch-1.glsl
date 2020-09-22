22b933eb8264f1750fa2ff64593b2ae1fa337ab6
t420babe <t420babe@gmail.com>
2020-09-21 22:13:17 -0400

    lib/t420babe/couch/couch-1.glsl

--- lib/t420babe/couch/couch-1.glsl
+++ lib/t420babe/couch/couch-1.glsl
@@ -51,7 +51,7 @@ float couch1_fbm (in vec2 st, peakamp audio) {
 
 void couch1(vec2 pos, float u_time, peakamp audio, out vec3 color) {
     // pos.y += sin(u_time * 0.5);
-    pos.x -= 1.0;
+    pos.x -= 2.0;
     pos /= 2.0;
 
     color += couch1_fbm(pos * 5.0, audio);
