39933388f1e1ce1d96246f2d50efeebc93338ab7
t420babe <t420babe@gmail.com>
2020-10-07 19:47:55 -0400

    dyka - do you know anything - techno prank by dubdogz

---
+++ lib/t420babe/dyka.glsl
@@ -0,0 +1,41 @@
+#ifndef T420BABE_DYKA
+#define T420BABE_DYKA
+
+#ifndef COMMON_PEAKAMP
+#include "./lib/common/peakamp.glsl"
+#endif
+
+#ifndef T420BABE_AUDIO_CIRCLE
+#include "./lib/t420babe/audio-circle.glsl"
+#endif
+
+#ifndef PXL_RAYS
+#include "./lib/pxl/rays-sdf.glsl"
+#endif
+
+#ifndef T420BABE_BABYDOYOUGETME
+#include "./lib/t420babe/babydoyougetme.glsl"
+#endif
+
+
+vec3 my_mix(peakamp audio) {
+  vec3 c1 = vec3(0.760704, 0.94902, 0.0);
+  vec3 c0 = vec3(0.94902 ,0.0, 0.760704);
+  float f = abs(audio.bandpass);
+  vec3 color = (1.0 - f) * c0 + f * c1;
+  return color;
+}
+
+void dyka(vec2 pos, float u_time, peakamp audio, out vec3 color) {
+  color = my_mix(audio);
+  color = babydoyougetme_1(pos, u_time, audio);
+
+  vec3 purp_circle =  vec3(0.0);
+  vec2 rayz_position = pos * 9.0 * audio.notch;
+
+  float rayz = rays_audio(rayz_position, 60, audio);
+  color *= rayz;
+  purple_circle_oh_yes_he_is_mio(pos, u_time, audio, purp_circle);
+  color += purp_circle;
+}
+#endif
