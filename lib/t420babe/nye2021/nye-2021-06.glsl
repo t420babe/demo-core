63004bea1b4d7898387d1601066eef3a19395db1
t420babe <t420babe@gmail.com>
2021-01-10 21:08:24 -0500

    the juul is just nostaligic now

---
+++ lib/t420babe/nye2021/nye-2021-06.glsl
@@ -0,0 +1,102 @@
+#ifndef T420BABE_NYE_2021_06
+#define T420BABE_NYE_2021_06
+
+#ifndef COMMON_PEAKAMP
+#include "./lib/common/peakamp.glsl"
+#endif
+
+#ifndef COMMON_PLOT
+#include "./lib/common/plot.glsl"
+#endif
+
+#ifndef COMMON_WRAP_TIME
+#include "./lib/common/wrap-time.glsl"
+#endif
+
+
+#ifndef COMMON_NOISE
+#include "./lib/common/noise.glsl"
+#endif
+
+vec2 hills_spherical_vortex(vec2 pos, float u_time) {
+  float a = 1.0;
+  float u_0 = 10.0;
+  float A = 15.0 / 2.0 * u_0 * 1.0 / pow(a, 2.0);
+
+  float u_int = 1.0 / 5.0 * A * pos.y * (pow(a, 2.0) - pow(pos.x, 2.0) - 2.0 * pow(pos.y, 2.0));
+  float v_int = 1.0 / 5.0 * A * pos.x * pos.y;
+
+  return vec2(u_int, v_int);
+}
+
+float vc_nyc(vec2 pos, float u_time, peakamp audio) {
+  pos = pos.yx;
+  pos.x += 5.0 * (sin(u_time * 0.5));
+  vec2 uv_int = hills_spherical_vortex(pos, u_time);
+  float z = (5.0 * uv_int.x + 1.0 * uv_int.y) / 5.0 + 2.0;
+
+  float time_wrap = wrap_time(u_time, 10.0);
+  z *= 10.0 * (time_wrap / 5.0);
+  float d = fract(z * 5.0);
+  if(mod(z, 2.0) > 1.0) d = 1.0 -d;
+
+  d = d / fwidth(z);
+  // d = (d);
+  return d;
+}
+
+
+float shape(vec2 pos, float radius, float u_time, peakamp audio) {
+  float r = length(pos / audio.highpass);
+  // float r = length(pos) * 2.0;
+  // float theta = atan(pos.y, pos.x);
+  float theta = pos.y  * pos.x + audio.notch;
+  float m = abs( mod( theta + u_time * 2.0, 3.14 * 2.0) - 3.14 ) / (3.14 * 2.0);
+  float f = radius;
+  float f1 = radius;
+
+  m += noise( pos + u_time * 0.1) * 0.5;
+  f += noise(pos + u_time * 1.0) * 0.1;
+  // f1 /= (theta * 50.0) * noise(pos + u_time * 1.0) * 0.05 * audio.bandpass;
+  // f1 += sin(theta * 20.0) * 0.1 * pow(m, 2.0);
+  f1 = vc_nyc(pos, u_time, audio);
+
+  // return 1.0 - smoothstep(f1, f + 1.007, r) / fwidth(f1);
+  return 1.0 - sharp(smoothstep(f1, f + 1.007, r) );
+}
+
+float shape_border(vec2 pos, float radius, float width, float u_time, peakamp audio) {
+  return shape(pos, radius, u_time, audio) - shape(pos, radius - width, u_time, audio);
+}
+
+vec3 nye_2021_06(vec2 pos, float u_time, peakamp audio) {
+  vec3 color = vec3(1.0);
+  audio.lowpass   *= 1.0;
+  audio.highpass  *= 1.0;
+  audio.bandpass  *= 1.0;
+  audio.notch     *= 1.0;
+
+
+  // shape_color_border(pos, 1.0, 0.10, u_time, audio, color);
+
+  // Color 0
+  color.b += audio.lowpass * 2.0;
+  color *= shape_border(pos, 3.0, 1.00, u_time, audio);
+  color.b *= abs(audio.lowpass * 2.0);
+  color.r -= abs(audio.lowpass * 2.0);
+
+  // color = color.bgr;
+  //
+  // Color 0
+  // // color.g += audio.lowpass * 2.0;
+  // color /= shape_border(pos * 0.8, 1.0, 5.10, u_time, audio);
+  // // color.b /= audio.lowpass * 1.0;
+  // color.b *= abs(audio.highpass) * 1.5;
+  // color.r /= abs(audio.notch) * 1.5;
+  // color.b += abs(audio.lowpass) * 1.5 - 0.29234;
+  // // color = color.rbg;
+
+  // color = 1.0 - color;
+  return color;
+}
+#endif
---
+++ lib/t420babe/nye2021/nye-2021-15.glsl
@@ -0,0 +1,297 @@
+#ifndef T420BABE_NYE_2021_15
+#define T420BABE_NYE_2021_15
+
+#ifndef COMMON_PEAKAMP
+#include "./lib/common/peakamp.glsl"
+#endif
+
+#ifndef COMMON_PLOT
+#include "./lib/common/plot.glsl"
+#endif
+
+#ifndef COMMON_WRAP_TIME
+#include "./lib/common/wrap-time.glsl"
+#endif
+
+#ifndef COMMON_MATH_CONSTANTS
+#include "./lib/common/math-constants.glsl"
+#endif
+
+float random (in vec2 _st) {
+    return fract(sin(dot(_st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
+}
+
+// Based on Morgan McGuire @morgan3d
+// https://www.shadertoy.com/view/4dS3Wd
+float noise_clouds (in vec2 _st) {
+    vec2 i = floor(_st);
+    vec2 f = fract(_st);
+
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    vec2 u = f * f * (3.0 - 2.0 * f);
+
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
+}
+
+// #define NUM_OCTAVES 5
+//
+
+float fbm ( in vec2 _st) {
+    float onum = 5.0;
+    float v = 0.0;
+    float a = 0.5;
+    vec2 shift = vec2(100.0);
+    // Rotate to reduce axial bias
+    mat2 rot = mat2(cos(0.5), sin(0.5),
+                    -sin(0.5), cos(0.50));
+    for (int i = 0; i < int(onum); ++i) {
+        v += a * noise_clouds(_st);
+        _st = rot * _st * 2.0 + shift;
+        a *= 0.5;
+    }
+    return v;
+}
+
+vec3 clouds(vec2 pos, float u_time, peakamp audio) {
+    // vec2 st = pos * abs(sin(u_time*0.1)*3.0);
+    vec2 st = pos;
+    vec3 color = vec3(0.0);
+
+    vec2 q = vec2(0.);
+    q.x = fbm( st + 0.00*u_time);
+    q.y = fbm( st + vec2(1.0));
+
+    vec2 r = vec2(0.);
+    r.x = fbm( st + 1.0*q + vec2(1.7,9.2)+ 0.15*u_time );
+    r.y = fbm( st + 1.0*q + vec2(8.3,2.8)+ 0.126*u_time);
+
+    float f = fbm(pos+r);
+
+    color = mix(vec3(0.101961,0.619608,0.666667),
+                vec3(0.666667,0.666667,0.498039),
+                clamp((f*f)*4.0,0.0,1.0));
+
+    color = mix(color,
+                vec3(0,0,0.164706),
+                clamp(length(q),0.0,1.0));
+
+    color = mix(color,
+                vec3(0.666667,1,1),
+                clamp(length(r.x),0.0,1.0));
+
+    return vec3((f*f*f+.6*f*f+.5*f)*color);
+}
+
+// Cellular noise ("Worley noise") in 3D in GLSL.
+// Copyright (c) Stefan Gustavson 2011-04-19. All rights reserved.
+// This code is released under the conditions of the MIT license.
+// See LICENSE file for details.
+
+// Permutation polynomial: (34x^2 + x) mod 289
+vec4 permute(vec4 x) {
+  return mod((34.0 * x + 1.0) * x, 289.0);
+}
+vec3 permute(vec3 x) {
+  return mod((34.0 * x + 1.0) * x, 289.0);
+}
+
+// Cellular noise, returning F1 and F2 in a vec2.
+// Speeded up by using 2x2x2 search window instead of 3x3x3,
+// at the expense of some pattern artifacts.
+// F2 is often wrong and has sharp discontinuities.
+// If you need a good F2, use the slower 3x3x3 version.
+vec2 cellular2x2x2(vec3 P) {
+	float K = 0.142857142857; // 1/7
+	float Ko = 0.428571428571; // 1/2-K/2
+	float K2 = 0.020408163265306; // 1/(7*7)
+	float Kz = 0.166666666667; // 1/6
+	float Kzo = 0.416666666667; // 1/2-1/6*2
+	float jitter = 0.8; // smaller jitter gives less errors in F2
+	vec3 Pi = mod(floor(P), 289.0);
+ 	vec3 Pf = fract(P);
+	vec4 Pfx = Pf.x + vec4(0.0, -1.0, 0.0, -1.0);
+	vec4 Pfy = Pf.y + vec4(0.0, 0.0, -1.0, -1.0);
+	vec4 p = permute(Pi.x + vec4(0.0, 1.0, 0.0, 1.0));
+	p = permute(p + Pi.y + vec4(0.0, 0.0, 1.0, 1.0));
+	vec4 p1 = permute(p + Pi.z); // z+0
+	vec4 p2 = permute(p + Pi.z + vec4(1.0)); // z+1
+	vec4 ox1 = fract(p1*K) - Ko;
+	vec4 oy1 = mod(floor(p1*K), 7.0)*K - Ko;
+	vec4 oz1 = floor(p1*K2)*Kz - Kzo; // p1 < 289 guaranteed
+	vec4 ox2 = fract(p2*K) - Ko;
+	vec4 oy2 = mod(floor(p2*K), 7.0)*K - Ko;
+	vec4 oz2 = floor(p2*K2)*Kz - Kzo;
+	vec4 dx1 = Pfx + jitter*ox1;
+	vec4 dy1 = Pfy + jitter*oy1;
+	vec4 dz1 = Pf.z + jitter*oz1;
+	vec4 dx2 = Pfx + jitter*ox2;
+	vec4 dy2 = Pfy + jitter*oy2;
+	vec4 dz2 = Pf.z - 1.0 + jitter*oz2;
+	vec4 d1 = dx1 * dx1 + dy1 * dy1 + dz1 * dz1; // z+0
+	vec4 d2 = dx2 * dx2 + dy2 * dy2 + dz2 * dz2; // z+1
+
+	// Sort out the two smallest distances (F1, F2)
+#if 1
+	// Cheat and sort out only F1
+	d1 = min(d1, d2);
+	d1.xy = min(d1.xy, d1.wz);
+	d1.x = min(d1.x, d1.y);
+	return sqrt(d1.xx);
+#else
+	// Do it right and sort out both F1 and F2
+	vec4 d = min(d1,d2); // F1 is now in d
+	d2 = max(d1,d2); // Make sure we keep all candidates for F2
+	d.xy = (d.x < d.y) ? d.xy : d.yx; // Swap smallest to d.x
+	d.xz = (d.x < d.z) ? d.xz : d.zx;
+	d.xw = (d.x < d.w) ? d.xw : d.wx; // F1 is now in d.x
+	d.yzw = min(d.yzw, d2.yzw); // F2 now not in d2.yzw
+	d.y = min(d.y, d.z); // nor in d.z
+	d.y = min(d.y, d.w); // nor in d.w
+	d.y = min(d.y, d2.x); // F2 is now in d.y
+	return sqrt(d.yz); // F1 and F2
+#endif
+}
+vec2 cellular2x2(vec2 P) {
+
+  float K = 0.142857142857;      // 1/7
+  float K2 = 0.0;    // K/2
+  float jitter = 0.01;
+
+  vec2 Pi = mod((P), 289.0);
+  vec2 Pf = fract(exp2(P));
+
+  vec4 Pfx = Pf.x - vec4(-0.5, -1.5, -0.5, -1.5);
+  vec4 Pfy = Pf.y - vec4(-0.5, -0.5, -1.5, -1.5);
+
+  vec4 p = permute(Pi.x + vec4(0.0, 1.0, 0.0, 1.0));
+  p = permute(p + Pi.y + vec4(0.0, 0.0, 1.0, 1.0));
+
+  // vec4 ox = mod(p, abs(2.0 * audio.notch)) * abs(tan(u_time) + 0.0) + K2;
+  vec4 ox = mod(p, abs(2.0 * audio.lowpass)) * (audio.notch);
+  // vec4 oy = mod(floor(p * K), 1.0) * K  + K2;
+  vec4 oy = mod(floor(p * K), 1.0) * K  + K2;
+
+  vec4 dx = Pfx + jitter * ox;
+  vec4 dy = Pfy + jitter * oy;
+
+  // d11, d12, d21 and d22, squared
+  vec4 d = dx * dx + dy * dy;
+
+  // Sort out the two smallest distances
+  #if 0
+    // Cheat and pick only F1
+    d.yx = max(d.wy, d.zw);
+    d.x = min(d.x, d.y);
+
+    return d.xx;                // F1 duplicated, F2 not computed
+
+  #else
+    // Do it right and find both F1 and F2
+    d.xy = (d.x < d.y) ? d.xy : d.yx; // Swap if smaller
+    d.xz = (d.x < d.z) ? d.xz : d.zx;
+    d.xw = (d.x < d.w) ? d.xw : d.wx;
+
+    d.y = min(d.y, d.z);
+    d.y = min(d.y, d.w);
+
+    return tan(d.xy);
+  #endif
+
+}
+
+
+#ifdef GL_OES_standard_derivatives
+#extension GL_OES_standard_derivatives : enable
+#endif
+float aastep(float threshold, float value) {
+    #ifdef GL_OES_standard_derivatives
+    float afwidth = 0.7 * length(vec2(dFdx(value), dFdy(value)));
+    return smoothstep(threshold-afwidth, threshold+afwidth, value);
+    #else
+    return step(threshold, value);
+    #endif
+}
+
+float rect_sdf(vec2 st, vec2 s) {
+    st = st*2.-1.;
+    return max( abs(st.x/s.x),
+                abs(st.y/s.y) );
+}
+
+float cross_sdf(vec2 st, float s) {
+    vec2 size = vec2(.25, s);
+    return min( rect_sdf(st.xy,size.xy),
+                rect_sdf(st.xy,size.yx));
+}
+
+float circle_sdf(vec2 st) {
+    return length(st-.5)*2.;
+}
+vec2 rotate(vec2 pos, float a, float offset_value) {
+  pos = mat2(cos(a), -sin(a), sin(a), cos(a)) * (pos - offset_value);
+  return pos + offset_value;
+}
+
+// 809fde9, main.frag green rooster with the sun sofia's theme
+void doppler_green_rooster(vec2 pos, float u_time, peakamp audio, out vec3 color) {
+
+  // pos.x += 0.40;
+  // pos.y += 0.50;
+  color = vec3(1.0, 0.1234, 0.34);
+  float pct = aastep(pos.y, -pos.y) * sin(u_time);
+  pct *= cross_sdf(rotate(pos, circle_sdf(vec2(pos.x, pos.x) * 0.5), 0.0), 0.4);
+  float pct2 = circle_sdf(pos);
+  color = vec3(pct * color + color * pct2);
+  // color.r = color.r * audio.highpass * 2.5;
+	color.b *= audio.notch;
+  color.g += audio.lowpass;
+}
+
+float spiral_pxl(vec2 st, float t) {
+    float r = dot(st.yx, st.yx);
+    float a = atan(st.y,st.x);
+    return abs(((fract(r) * t / a * 1.000)));
+}
+
+vec3 nye_2021_15(vec2 pos, float u_time, peakamp audio) {
+  // pos = pos.yx;
+  vec3 color = vec3(1.0);
+  audio.lowpass   *= 1.0;
+  audio.highpass  *= 1.0;
+  audio.bandpass  *= 1.0;
+  audio.notch     *= 1.0;
+
+  vec2 st = pos;
+  st *= 25.0 * abs(sin(u_time * 0.01));
+  st += 1.0;
+	vec2 F = cellular2x2x2(vec3(st * 1.0, u_time));
+	float n = smoothstep(0.0, abs(sin(u_time * 0.05)) + 1.0, F.x) / ( abs(audio.notch));
+  // n = step(n, sin(pos.x));
+  color = vec3(n);
+  color += spiral_pxl(pos.yx * 5.5 * abs(audio.bandpass), wrap_time(u_time, 10.0) + 10.0);
+  color.b *= 1.053 / abs(audio.lowpass);
+  // color.b -= 0.4;
+  color.r *= 0.4 * abs(audio.highpass);
+  color = color.brg;
+  // color.g /= 0.4;
+  color.b *= abs(audio.highpass);
+  color = vec3(0.0, 0.5, 1.0) * color;
+  vec3 clouds_color = vec3(1.0);
+  clouds_color = clouds(pos, u_time, audio);
+  color *= clouds_color;
+
+  // color = 1.0 - color;
+
+
+  return color;
+}
+#endif
