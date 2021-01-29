5c3604d22371c699706eaa1ab1be36e57a13a61c
t420babe <t420babe@gmail.com>
2021-01-28 20:40:50 -0500

    void -> vec3

--- lib/t420babe/misc/misx-07.glsl
+++ lib/t420babe/misc/misx-07.glsl
@@ -1,4 +1,4 @@
-// #effect #effectshape #fav5 #shadershoot
+// #effect #effectshape #fav6 #shadershoot
 #ifndef T420BABE_MISX_07
 #define T420BABE_MISX_07
 
@@ -22,7 +22,7 @@
 #include "./lib/t420babe/vortex-contour.glsl"
 #endif
 
-float shape(vec2 pos, float radius, float u_time, peakamp audio) {
+float misx_07_shape(vec2 pos, float radius, float u_time, peakamp audio) {
   float r = length(pos / audio.highpass);
   // float r = length(pos) * 2.0;
   // float theta = atan(pos.y, pos.x);
@@ -44,29 +44,34 @@ float shape(vec2 pos, float radius, float u_time, peakamp audio) {
   // return tt;
 }
 
-float shape_border(vec2 pos, float radius, float width, float u_time, peakamp audio) {
-  return shape(pos, radius, u_time, audio) - shape(pos, radius - width, u_time, audio);
+float misx_07_shape_border(vec2 pos, float radius, float width, float u_time, peakamp audio) {
+  return misx_07_shape(pos, radius, u_time, audio) - misx_07_shape(pos, radius - width, u_time, audio);
 }
 
-void misx_07(vec2 pos, float u_time, peakamp audio, inout vec3 color) {
+vec3 misx_07(vec2 pos, float u_time, peakamp audio) {
+  vec3 color = vec3(1.0);
   audio.lowpass   *= 0.1;
   audio.highpass  *= 1.0;
   audio.bandpass  *= 2.0;
   audio.notch     *= 1.0;
 
-  // shape_color_border(pos, 1.0, 0.10, u_time, audio, color);
+  // misx_07_shape_color_border(pos, 1.0, 0.10, u_time, audio, color);
 
   // // Color 0
   // color.b += audio.lowpass * 2.0;
-  // color *= shape_border(pos, 1.0, 0.10, u_time, audio);
+  // color *= misx_07_shape_border(pos, 1.0, 0.10, u_time, audio);
   // color.g *= audio.lowpass * 2.0;
 
   // Color 0
   color.g += audio.lowpass * 2.0;
-  color *= shape_border(pos, 1.0, 0.01, u_time, audio);
+  color *= misx_07_shape_border(pos, 1.0, 0.01, u_time, audio);
   color.g *= audio.lowpass * 2.0;
   color.r *= audio.lowpass * 1.0;
 
-  color = 1.0 - color;
+  color = color.bgr;
+  // color.r /= abs(audio.notch) * 15.0;
+
+  // color = 1.0 - color;
+  return color;
 }
 #endif
