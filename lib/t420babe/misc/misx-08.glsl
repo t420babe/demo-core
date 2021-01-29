d72c39e212401bb97c2da7e948386fbb43bfbb9c
t420babe <t420babe@gmail.com>
2021-01-28 21:15:04 -0500

    void -> vec3

--- lib/t420babe/misc/misx-08.glsl
+++ lib/t420babe/misc/misx-08.glsl
@@ -1,4 +1,4 @@
-// #effect #effectshape #fav5 #shadershoot #show
+// #effect #effectmisx_08_shape #fav4 #shadershoot #show
 #ifndef T420BABE_MISX_08
 #define T420BABE_MISX_08
 
@@ -22,7 +22,7 @@
 #include "./lib/t420babe/vortex-contour.glsl"
 #endif
 
-float shape(vec2 pos, float radius, float u_time, peakamp audio) {
+float misx_08_shape(vec2 pos, float radius, float u_time, peakamp audio) {
   float r = length(pos / audio.highpass);
   // float r = length(pos) * 2.0;
   // float theta = atan(pos.y, pos.x);
@@ -44,30 +44,32 @@ float shape(vec2 pos, float radius, float u_time, peakamp audio) {
   // return tt;
 }
 
-float shape_border(vec2 pos, float radius, float width, float u_time, peakamp audio) {
-  return shape(pos, radius, u_time, audio) - shape(pos, radius - width, u_time, audio);
+float misx_08_shape_border(vec2 pos, float radius, float width, float u_time, peakamp audio) {
+  return misx_08_shape(pos, radius, u_time, audio) - misx_08_shape(pos, radius - width, u_time, audio);
 }
 
-void misx_08(vec2 pos, float u_time, peakamp audio, inout vec3 color) {
+vec3 misx_08(vec2 pos, float u_time, peakamp audio) {
+  vec3 color = vec3(1.0);
   audio.lowpass   *= 0.1;
   audio.highpass  *= 1.0;
   audio.bandpass  *= 2.0;
   audio.notch     *= 1.0;
 
-  // shape_color_border(pos, 1.0, 0.10, u_time, audio, color);
+  // misx_08_shape_color_border(pos, 1.0, 0.10, u_time, audio, color);
 
   // Color 0
   color.b += audio.lowpass * 2.0;
-  color *= shape_border(pos, 1.0, 0.10, u_time, audio);
+  color *= misx_08_shape_border(pos, 1.0, 0.10, u_time, audio);
   color.g *= audio.lowpass * 2.0;
 
   // Color 0
   // color.g += audio.lowpass * 2.0;
-  // color *= shape_border(pos, 1.0, 0.01, u_time, audio);
+  // color *= misx_08_shape_border(pos, 1.0, 0.01, u_time, audio);
   // color.g *= audio.lowpass * 2.0;
   // color.r *= audio.lowpass * 1.0;
 
   // color = 1.0 - color;
+  return color;
 }
 #endif
 
