109add012e1a5928e6f4ef4e1fbc484d2fa68e25
t420babe <t420babe@gmail.com>
2021-01-28 20:50:04 -0500

    void -> vec3

--- lib/t420babe/misc/misx-12.glsl
+++ lib/t420babe/misc/misx-12.glsl
@@ -1,4 +1,4 @@
-// #effect #effectshape #fav5 #shadershoot #corey
+// #effect #effectmisx_12_shape #fav5 #shadershoot #corey
 #ifndef T420BABE_MISX_12
 #define T420BABE_MISX_12
 
@@ -22,7 +22,15 @@
 #include "./lib/t420babe/vortex-contour.glsl"
 #endif
 
-float shape(vec2 pos, float radius, float u_time, peakamp audio) {
+// #ifndef PXL_ROTATE
+// #include "./lib/pxl/rotate-sdf.glsl"
+// #endif
+//
+mat2 rot(float theta){
+    return mat2(cos(theta), -sin(theta), sin(theta), cos(theta));
+}
+float misx_12_shape(vec2 pos, float radius, float u_time, peakamp audio) {
+
   float r = length(pos / audio.highpass);
   // float r = length(pos) * 2.0;
   // float theta = atan(pos.y, pos.x);
@@ -45,29 +53,37 @@ float shape(vec2 pos, float radius, float u_time, peakamp audio) {
   // return tt;
 }
 
-float shape_border(vec2 pos, float radius, float width, float u_time, peakamp audio) {
-  return shape(pos, radius, u_time, audio) - shape(pos, radius - width, u_time, audio);
+float misx_12_shape_border(vec2 pos, float radius, float width, float u_time, peakamp audio) {
+  return misx_12_shape(pos, radius, u_time, audio) - misx_12_shape(pos, radius - width, u_time, audio);
 }
 
-void misx_12(vec2 pos, float u_time, peakamp audio, inout vec3 color) {
+vec3 misx_12(vec2 pos, float u_time, peakamp audio) {
+  vec3 color = vec3(1.0);
   audio.lowpass   *= 0.1;
   audio.highpass  *= 1.0;
   audio.bandpass  *= 2.0;
   audio.notch     *= 1.0;
+  pos = pos.yx;
+  pos *= 1.0;
 
-  // shape_color_border(pos, 1.0, 0.10, u_time, audio, color);
+  // misx_12_shape_color_border(pos, 1.0, 0.10, u_time, audio, color);
 
   // Color 0
-  color /= shape_border(pos, 1.0, 0.10, u_time, audio);
-  color.r /= sin(audio.lowpass * 0.5);
+  color /= misx_12_shape_border(pos, 1.0, 0.10, u_time, audio);
+  color.b /= sin(audio.lowpass * 0.5);
   color.b *= audio.lowpass * 1.0;
 
   // // Color 0
-  // color.g += audio.lowpass * 2.0;
-  // color *= shape_border(pos, 1.0, 0.01, u_time, audio);
-  // color.g *= audio.lowpass * 2.0;
-  // color.r *= audio.lowpass * 1.0;
+  color.g += audio.lowpass * 10.0;
+  color *= misx_12_shape_border(pos, 1.0, 0.01, u_time, audio);
+  color.g *= audio.lowpass * 10.0;
+  color.r *= audio.lowpass * 10.0;
 
+  // color = color.bgr;
+  // color = color.gbr;
+  color.b *= 2.0;
+  color.g *= abs(sin(u_time));
   // color = 1.0 - color;
+  return color;
 }
 #endif
