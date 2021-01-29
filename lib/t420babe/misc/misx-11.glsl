a6fb165c03f4022d8538acb3a54732abde60d5eb
t420babe <t420babe@gmail.com>
2021-01-28 21:19:03 -0500

    void -> vec3

--- lib/t420babe/misc/misx-11.glsl
+++ lib/t420babe/misc/misx-11.glsl
@@ -1,4 +1,4 @@
-// #effect #effectshape #fav5 #shadershoot #corey
+// #effect #effectmisx_11_shape #fav4 #shadershoot #corey
 #ifndef T420BABE_MISX_11
 #define T420BABE_MISX_11
 
@@ -22,7 +22,7 @@
 #include "./lib/t420babe/vortex-contour.glsl"
 #endif
 
-float shape(vec2 pos, float radius, float u_time, peakamp audio) {
+float misx_11_shape(vec2 pos, float radius, float u_time, peakamp audio) {
   float r = length(pos / audio.highpass);
   // float r = length(pos) * 2.0;
   // float theta = atan(pos.y, pos.x);
@@ -44,8 +44,8 @@ float shape(vec2 pos, float radius, float u_time, peakamp audio) {
   // return tt;
 }
 
-float shape_border(vec2 pos, float radius, float width, float u_time, peakamp audio) {
-  return shape(pos, radius, u_time, audio) - shape(pos, radius - width, u_time, audio);
+float misx_11_shape_border(vec2 pos, float radius, float width, float u_time, peakamp audio) {
+  return misx_11_shape(pos, radius, u_time, audio) - misx_11_shape(pos, radius - width, u_time, audio);
 }
 
 vec3 misx_11(vec2 pos, float u_time, peakamp audio) {
@@ -55,16 +55,16 @@ vec3 misx_11(vec2 pos, float u_time, peakamp audio) {
   audio.bandpass  *= 2.0;
   audio.notch     *= 1.0;
 
-  // shape_color_border(pos, 1.0, 0.10, u_time, audio, color);
+  // misx_11_shape_color_border(pos, 1.0, 0.10, u_time, audio, color);
 
   // Color 0
   // color.b += audio.lowpass * 2.0;
-  color *= shape_border(pos, 1.0, 0.10, u_time, audio);
+  color *= misx_11_shape_border(pos, 1.0, 0.10, u_time, audio);
   color.g *= audio.lowpass * 2.0;
 
   // // Color 0
   // color.g += audio.lowpass * 2.0;
-  // color *= shape_border(pos, 1.0, 0.01, u_time, audio);
+  // color *= misx_11_shape_border(pos, 1.0, 0.01, u_time, audio);
   // color.g *= audio.lowpass * 2.0;
   // color.r *= audio.lowpass * 1.0;
 
