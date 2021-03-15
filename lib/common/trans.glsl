807bef619c17153815af88687d36644b29dfc03a
t420babe <t420babe@gmail.com>
2021-03-15 19:44:09 -0400

    Add example use case

--- lib/common/trans.glsl
+++ lib/common/trans.glsl
@@ -23,3 +23,14 @@ vec3 trans(float time, float start, float end, vec3 color_0, vec3 color_1) {
 }
 #endif
 
+// Example usage
+// vec3 color = vec3(1.0);
+// vec3 color_0 = vec3(1.0);
+// vec3 color_1 = vec3(1.0);
+//
+// float t;
+// t = wrap_time(u_at * 1.0, 1.0);
+//
+// color_0 = algo_00(pos, u_at, audio, u_resolution);
+// color_1 = algo_01(pos, u_at, audio, u_resolution);
+// color = mix(color_1, color_0, t);
