a42a3a8f6f135bc7132acc67bfc5da2135417d90
t420babe <t420babe@gmail.com>
2021-06-01 12:50:40 -0400

    Landing in Miami

--- lib/t420babe/b2b/b2b-39.glsl
+++ lib/t420babe/b2b/b2b-39.glsl
@@ -1,6 +1,6 @@
 // #chill 
-#ifndef T4B_B2B_41
-#define T4B_B2B_41
+#ifndef T4B_B2B_39
+#define T4B_B2B_39
 
 #ifndef PXL_ROTATE
 #include "./lib/pxl/rotate-sdf.glsl"
@@ -13,24 +13,24 @@
 #ifndef COMMON_COMMON
 #include "lib/common/00-common.glsl"
 #endif
-mat2 b2b_41_rotate2d(float a){
+mat2 b2b_39_rotate2d(float a){
   float c=cos(a), s=sin(a);
   return mat2(c,-s,s,c);
 }
 
-float b2b_41_blob(vec3 p3, float time){
+float b2b_39_blob(vec3 p3, float time){
   p3 = p3.yxz;
   p3.xz *= rotate2d(time * 0.4);
   p3.xy *= rotate2d(time * 0.3);
   vec3 q = p3 * 2.0 + time;
-  float x0 = length( asin(p3) + vec3( sin(time * 0.7) ) );
+  float x0 = length( sin(p3) + vec3( sin(time * 0.7) ) );
   float x1 = ( length(p3) + p3.x  * p3.y);
   float x2 = ( q.x + ( length(q) ) ) * 0.5 - 10.0;
   return x0 * x1 + x2;
 }
 
 
-void b2b_41(vec3 p3, float time, peakamp audio) {
+void b2b_39(vec3 p3, float time, peakamp audio) {
   // audio.lowpass = 0.05;
   // audio.highpass = 0.05;
   // audio.bandpass = 0.05;
@@ -38,18 +38,17 @@ void b2b_41(vec3 p3, float time, peakamp audio) {
   // time += 50.0;
   vec3 color = vec3(1.0);
   // p3.y *= 0.75;
-  p3.xz *= rotate2d(time * 1.25);
-  p3.xy *= rotate2d(time * 0.55);
-  // p3 *= 200.0;
-  p3 *= 1.5;
-  float y = (sin(p3.x) * fract(p3.y * time * PI));
-  float m = plot(vec2(p3), y, 0.15) * audio.notch * 2.0;
+  p3.xz *= rotate2d(time * 0.25);
+  p3.xy *= rotate2d(time * 0.25);
+  p3 *= 200.0;
+  float y = (tan(p3.x) * cos(p3.x * time * 3.0));
+  float m = plot(vec2(p3), y, 0.25) * 1.0;
 
   // p3.x -= 0.5;
-  float rz = b2b_41_blob(p3, time);
-  float f = ( rz * b2b_41_blob(p3, y) ) * 1.0;
-  vec3 l = vec3(1.0) + vec3(abs(audio.lowpass), abs(audio.bandpass), abs(audio.notch)) * rz;
-  color = vec3(2.0 / m);
+  float rz = b2b_39_blob(p3, time);
+  float f = ( rz * b2b_39_blob(p3, y) ) * 1.0;
+  vec3 l = vec3(0.0) + vec3(abs(audio.lowpass), abs(audio.bandpass), abs(audio.notch)) * rz;
+  // color = vec3(1.0 / m);
   color *= l;
   // color += (smoothstep(0.5, 0.5, rz) );
 
@@ -65,7 +64,7 @@ void b2b_41(vec3 p3, float time, peakamp audio) {
   // color = rgb2hsv(1.0 - color.brg);
   // color = rgb2hsv(color.bgr);
   color = rgb2hsv(color);
-  // color = color.bgr;
+  color = color.bgr;
   // color /= 10.0;
   // color *= 2.0;
   // gl_FragColor = vec4( 1.0 - color.grb, 1.0);
