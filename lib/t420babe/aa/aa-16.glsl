f3a55965310ecb4b539131abd770465789918000
RJ Rybarczyk <rj@rybar.tech>
2021-07-18 02:25:51 -0400

    aa-16

--- lib/t420babe/aa/aa-16.glsl
+++ lib/t420babe/aa/aa-16.glsl
@@ -33,30 +33,11 @@ vec3 aa_16_1(vec3 p3, peakamp audio) {
   vec3 color_a = vec3(0.234, 0.5345, 0.0984);
   vec3 color_b = vec3(0.534, 0.1345, 0.8984);
   return gradient_and_line(pos, fx0, fx1, color_b, COLOR_B);
-
-  // return color;
 }
 
 void aa_16(vec3 p3, peakamp audio) {
   vec3 color = aa_16_1(p3, audio);
-
   gl_FragColor = vec4(color, 1.0);
-
-  // return color;
 }
-
-// void main() {
-//   vec2 pos = gl_FragCoord.xy / u_resolution.xy;
-//   // vec2 pos = (2.0 * gl_FragCoord.xy - u_resolution.xy) / u_resolution.y;
-//   vec3 color = vec3(0.2);
-//
-//   if (audio.notch > 6.0) {
-//     color = current2(pos) * audio.notch;
-//   } else {
-//     color = current(pos);
-//   }
-//
-//   gl_FragColor = vec4(color, 1.0);
-// }
 
 #endif
