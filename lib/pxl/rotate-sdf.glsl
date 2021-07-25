9210409e0ccc7af3136539378978946493e17ea9
RJ Rybarczyk <rj@rybar.tech>
2021-07-25 19:33:02 -0400

    Add rotate w offet from transform lib file

--- lib/pxl/rotate-sdf.glsl
+++ lib/pxl/rotate-sdf.glsl
@@ -5,6 +5,11 @@ vec2 rotate(vec2 pos, float theta) {
     return pos + 0.5;
 }
 
+vec2 rotate_w_offset(vec2 pos, float a, float offset_value) {
+  pos = mat2(cos(a), -sin(a), sin(a), cos(a)) * (pos - offset_value);
+  return pos + offset_value;
+}
+
 // pos = rotate2d(sin(u_time) * 3.14 / 1.0) * pos;
 mat2 rotate2d(float theta){
     return mat2(cos(theta), -sin(theta), sin(theta), cos(theta));
