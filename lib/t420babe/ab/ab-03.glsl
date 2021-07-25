b503a1b5782896925b0ed6f3d547d2c5df3e830e
RJ Rybarczyk <rj@rybar.tech>
2021-07-25 18:11:37 -0400

    Pattern bgm

--- lib/t420babe/ab/ab-03.glsl
+++ lib/t420babe/ab/ab-03.glsl
@@ -1,7 +1,6 @@
 // #effect #fav1 #shadershoot
-#ifndef T420BABE_TRYIT_00
-#define T420BABE_TRYIT_00
-#ifndef T420BABE_TRYIT_01
+#ifndef T4B_AB_03
+#define T4B_AB_03
 
 #ifndef COMMON_PEAKAMP
 #include "./lib/common/peakamp.glsl"
@@ -26,27 +25,27 @@ float fillY(vec2 _pos, float _pct,float _antia){
   // return  smoothstep( _pct-_antia, _pct, _pos.y);
 }
 
-float _zigzag(vec2 pos, float u_time, peakamp audio) {
+float _zigzag(vec2 pos, float time, peakamp audio) {
 
   float zoom = 35.0;
   pos = mirror_tile(pos*vec2(1.,2.), zoom);
   float x = pos.x*2.;
   float a = sin(1.+sin(x*0.14));
-  float b = sin(1.+cos((x+1.)*3.14 * sin(u_time)));
-  float f = sin(x * u_time);
+  float b = sin(1.+cos((x+1.)*3.14 * sin(time)));
+  float f = sin(x * time);
 
   return fillY(pos,mix(a,b,f),0.01) ;
 }
-void zigzag(vec2 pos, float u_time, peakamp audio, inout vec3 color){
-  color = vec3( _zigzag(pos, u_time, audio));
+void zigzag(vec2 pos, float time, peakamp audio, inout vec3 color){
+  color = vec3( _zigzag(pos, time, audio));
 }
 
 float circle_1(vec2 st, float radius) {
     return length(st) * radius;
 }
-#endif
 
-vec3 tryit_00(vec2 pos, float u_time, peakamp audio) {
+void ab_03(vec3 p3, float time, peakamp audio) {
+  vec2 pos = p3.xy;
   vec3 color = vec3(1.0);
   pos /= 1.5;
   pos.y -= 0.5;
@@ -54,18 +53,18 @@ vec3 tryit_00(vec2 pos, float u_time, peakamp audio) {
 
   float c_pct = circle_1(pos, abs(audio.notch) / 2.5);
 
-  // color.r = _zigzag(pos, u_time, audio);
+  // color.r = _zigzag(pos, time, audio);
   vec3 zz_color = vec3(1.0);
-  zigzag(pos, u_time, audio, zz_color);
+  zigzag(pos, time, audio, zz_color);
   color.g *= 0.5 * abs(audio.bandpass) + zz_color.b;
   color.r = abs(tan(audio.bandpass)) + zz_color.r * 0.5;
-  color.b = abs(sin(u_time));
+  color.b = abs(sin(time));
   color.b *= zz_color.r + audio.bandpass;
   // color += sharp(c_pct);
   // color /= c_pct;
   // color /= zz_color;
 
-  return color;
+  gl_FragColor = vec4(color, 1.0);
 }
 
 
