03bf56c5f6343302d27ae33b54c7a72c70badb59
RJ Rybarczyk <rj@rybar.tech>
2021-08-15 18:55:31 -0400

    Rename b2b -> bg

--- lib/t420babe/bj/bj-00.glsl
+++ lib/t420babe/bj/bj-00.glsl
@@ -1,5 +1,5 @@
-#ifndef T4B_LLL_00
-#define T4B_LLL_00
+#ifndef T4B_BJ_00
+#define T4B_BJ_00
 
 #ifndef COMMON_COMMON
 #include "lib/common/00-common.glsl"
@@ -13,13 +13,14 @@
 #include "lib/pxl/rotate-sdf.glsl"
 #endif
 
-vec3 lag_00(vec2 pos, float time, peakamp audio) {
+void bj_00(vec3 p3, float time, peakamp audio) {
+  vec2 pos = p3.xy;
   vec3 color = vec3(0.5, 0.0, 1.0);
   color = flash_mul(color, time, 5.0 + abs(audio.highpass));
   pos = rotate2d(time * 5.0 * 10.0) * pos;
   float poly = sharp(polygon(pos, 5, 5.0 * audio.bandpass));
   color *= poly;
 
-  return color;
+  gl_FragColor = vec4(color, 1.0);
 }
 #endif
