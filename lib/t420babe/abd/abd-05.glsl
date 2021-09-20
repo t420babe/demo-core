a6cc3bfc46c057b7fceb8bd16aba1e53cb456d2e
t420babe <t420babe@gmail.com>
2021-09-20 18:48:49 -0400

    nye2020bb -> ABD

--- lib/t420babe/abd/abd-05.glsl
+++ lib/t420babe/abd/abd-05.glsl
@@ -1,5 +1,5 @@
-#ifndef T420BABE_NYC2020BB_05
-#define T420BABE_NYC2020BB_05
+#ifndef T4B_ABD_05
+#define T4B_ABD_05
 
 #ifndef COMMON_PEAKAMP
 #include "./lib/common/peakamp.glsl"
@@ -13,202 +13,9 @@
 #include "./lib/common/math-constants.glsl"
 #endif
 
-// Cellular noise ("Worley noise") in 3D in GLSL.
-// Copyright (c) Stefan Gustavson 2011-04-19. All rights reserved.
-// This code is released under the conditions of the MIT license.
-// See LICENSE file for details.
-
-// Permutation polynomial: (34x^2 + x) mod 289
-vec4 permute(vec4 x) {
-  return mod((34.0 * x + 1.0) * x, 289.0);
-}
-vec3 permute(vec3 x) {
-  return mod((34.0 * x + 1.0) * x, 289.0);
-}
-
-// Cellular noise, returning F1 and F2 in a vec2.
-// Speeded up by using 2x2x2 search window instead of 3x3x3,
-// at the expense of some pattern artifacts.
-// F2 is often wrong and has sharp discontinuities.
-// If you need a good F2, use the slower 3x3x3 version.
-vec2 cellular2x2x2(vec3 P) {
-	float K = 0.142857142857; // 1/7
-	float Ko = 0.428571428571; // 1/2-K/2
-	float K2 = 0.020408163265306; // 1/(7*7)
-	float Kz = 0.166666666667; // 1/6
-	float Kzo = 0.416666666667; // 1/2-1/6*2
-	float jitter = 0.8; // smaller jitter gives less errors in F2
-	vec3 Pi = mod(floor(P), 289.0);
- 	vec3 Pf = fract(P);
-	vec4 Pfx = Pf.x + vec4(0.0, -1.0, 0.0, -1.0);
-	vec4 Pfy = Pf.y + vec4(0.0, 0.0, -1.0, -1.0);
-	vec4 p = permute(Pi.x + vec4(0.0, 1.0, 0.0, 1.0));
-	p = permute(p + Pi.y + vec4(0.0, 0.0, 1.0, 1.0));
-	vec4 p1 = permute(p + Pi.z); // z+0
-	vec4 p2 = permute(p + Pi.z + vec4(1.0)); // z+1
-	vec4 ox1 = fract(p1*K) - Ko;
-	vec4 oy1 = mod(floor(p1*K), 7.0)*K - Ko;
-	vec4 oz1 = floor(p1*K2)*Kz - Kzo; // p1 < 289 guaranteed
-	vec4 ox2 = fract(p2*K) - Ko;
-	vec4 oy2 = mod(floor(p2*K), 7.0)*K - Ko;
-	vec4 oz2 = floor(p2*K2)*Kz - Kzo;
-	vec4 dx1 = Pfx + jitter*ox1;
-	vec4 dy1 = Pfy + jitter*oy1;
-	vec4 dz1 = Pf.z + jitter*oz1;
-	vec4 dx2 = Pfx + jitter*ox2;
-	vec4 dy2 = Pfy + jitter*oy2;
-	vec4 dz2 = Pf.z - 1.0 + jitter*oz2;
-	vec4 d1 = dx1 * dx1 + dy1 * dy1 + dz1 * dz1; // z+0
-	vec4 d2 = dx2 * dx2 + dy2 * dy2 + dz2 * dz2; // z+1
-
-	// Sort out the two smallest distances (F1, F2)
-#if 1
-	// Cheat and sort out only F1
-	d1 = min(d1, d2);
-	d1.xy = min(d1.xy, d1.wz);
-	d1.x = min(d1.x, d1.y);
-	return sqrt(d1.xx);
-#else
-	// Do it right and sort out both F1 and F2
-	vec4 d = min(d1,d2); // F1 is now in d
-	d2 = max(d1,d2); // Make sure we keep all candidates for F2
-	d.xy = (d.x < d.y) ? d.xy : d.yx; // Swap smallest to d.x
-	d.xz = (d.x < d.z) ? d.xz : d.zx;
-	d.xw = (d.x < d.w) ? d.xw : d.wx; // F1 is now in d.x
-	d.yzw = min(d.yzw, d2.yzw); // F2 now not in d2.yzw
-	d.y = min(d.y, d.z); // nor in d.z
-	d.y = min(d.y, d.w); // nor in d.w
-	d.y = min(d.y, d2.x); // F2 is now in d.y
-	return sqrt(d.yz); // F1 and F2
+#ifndef CELLULAR_NOISE_2_X_2_X_2
+#include "lib/cellular-noise/cellular-2x2x2.glsl"
 #endif
-}
-vec2 cellular2x2(vec2 P) {
-
-  float K = 0.142857142857;      // 1/7
-  float K2 = 0.0;    // K/2
-  float jitter = 0.01;
-
-  vec2 Pi = mod((P), 289.0);
-  vec2 Pf = fract(exp2(P));
-
-  vec4 Pfx = Pf.x - vec4(-0.5, -1.5, -0.5, -1.5);
-  vec4 Pfy = Pf.y - vec4(-0.5, -0.5, -1.5, -1.5);
-
-  vec4 p = permute(Pi.x + vec4(0.0, 1.0, 0.0, 1.0));
-  p = permute(p + Pi.y + vec4(0.0, 0.0, 1.0, 1.0));
-
-  // vec4 ox = mod(p, abs(2.0 * audio.notch)) * abs(tan(u_time) + 0.0) + K2;
-  vec4 ox = mod(p, abs(2.0 * audio.lowpass)) * (audio.notch);
-  // vec4 oy = mod(floor(p * K), 1.0) * K  + K2;
-  vec4 oy = mod(floor(p * K), 1.0) * K  + K2;
-
-  vec4 dx = Pfx + jitter * ox;
-  vec4 dy = Pfy + jitter * oy;
-
-  // d11, d12, d21 and d22, squared
-  vec4 d = dx * dx + dy * dy;
-
-  // Sort out the two smallest distances
-  #if 0
-    // Cheat and pick only F1
-    d.yx = max(d.wy, d.zw);
-    d.x = min(d.x, d.y);
-
-    return d.xx;                // F1 duplicated, F2 not computed
-
-  #else
-    // Do it right and find both F1 and F2
-    d.xy = (d.x < d.y) ? d.xy : d.yx; // Swap if smaller
-    d.xz = (d.x < d.z) ? d.xz : d.zx;
-    d.xw = (d.x < d.w) ? d.xw : d.wx;
-
-    d.y = min(d.y, d.z);
-    d.y = min(d.y, d.w);
-
-    return tan(d.xy);
-  #endif
-
-}
-
-float random (in float x) {
-    return fract(sin(x)*1e4);
-}
-
-float random (in vec2 st) {
-    return fract(sin(dot(st.xy, vec2(12.9898,78.233)))* 43758.5453123);
-}
-
-float randomSerie(float x, float freq, float t) {
-    return step(.8,random( floor(x*freq)-floor(t) ));
-}
-
-vec3 ikeda(vec2 pos, float time) {
-    float cols = 1.5;
-    // float freq = random(floor(time)) + abs(atan(time) * 0.1);
-    // float freq = random(floor(time));
-    float freq = 1.0;
-    float t = 60. + time * (1.0 - freq) * 30.;
-
-    if (fract(cols * 0.5) < 0.5){
-        t *= -1.0;
-    }
-    t = 60.0;
-
-    // freq += random(floor(pos.y));
-
-    float offset = 0.025;
-    return vec3(randomSerie(sin(pos.x), freq * 100.0, t + offset),
-                 randomSerie(tan(pos.x), freq * 100.0, t),
-                 randomSerie(cos(pos.x), freq * 100.0, t - offset));
-
-}
-
-#ifdef GL_OES_standard_derivatives
-#extension GL_OES_standard_derivatives : enable
-#endif
-float aastep(float threshold, float value) {
-    #ifdef GL_OES_standard_derivatives
-    float afwidth = 0.7 * length(vec2(dFdx(value), dFdy(value)));
-    return smoothstep(threshold-afwidth, threshold+afwidth, value);
-    #else
-    return step(threshold, value);
-    #endif
-}
-
-float rect_sdf(vec2 st, vec2 s) {
-    st = st*2.-1.;
-    return max( abs(st.x/s.x),
-                abs(st.y/s.y) );
-}
-
-float cross_sdf(vec2 st, float s) {
-    vec2 size = vec2(.25, s);
-    return min( rect_sdf(st.xy,size.xy),
-                rect_sdf(st.xy,size.yx));
-}
-
-float circle_sdf(vec2 st) {
-    return length(st-.5)*2.;
-}
-vec2 rotate(vec2 pos, float a, float offset_value) {
-  pos = mat2(cos(a), -sin(a), sin(a), cos(a)) * (pos - offset_value);
-  return pos + offset_value;
-}
-
-// 809fde9, main.frag green rooster with the sun sofia's theme
-void doppler_green_rooster(vec2 pos, float u_time, peakamp audio, out vec3 color) {
-
-  // pos.x += 0.40;
-  // pos.y += 0.50;
-  color = vec3(1.0, 0.1234, 0.34);
-  float pct = aastep(pos.y, -pos.y) * sin(u_time);
-  pct *= cross_sdf(rotate(pos, circle_sdf(vec2(pos.x, pos.x) * 0.5), 0.0), 0.4);
-  float pct2 = circle_sdf(pos);
-  color = vec3(pct * color + color * pct2);
-  // color.r = color.r * audio.highpass * 2.5;
-	color.b *= audio.notch;
-  color.g += audio.lowpass;
-}
 
 float spiral_pxl(vec2 st, float t) {
     float r = dot(st.yx, st.yx);
@@ -216,20 +23,21 @@ float spiral_pxl(vec2 st, float t) {
     return abs(((fract(r) * t / a * 1.000)));
 }
 
-vec3 nyc2020bb_05(vec2 pos, float u_time, peakamp audio) {
+void abd_05(vec3 p3, float time, peakamp audio) {
+  vec2 pos = p3.xy;
   vec3 color = vec3(1.0);
   audio.lowpass   *= 1.0;
   audio.highpass  *= 1.0;
   audio.bandpass  *= 1.0;
   audio.notch     *= 1.0;
   vec2 st = pos;
-  st *= 25.0 * abs(sin(u_time * 0.01));
+  st *= 25.0 * abs(sin(time * 0.01));
   st += 1.0;
-	vec2 F = cellular2x2x2(vec3(st * 1.0, u_time));
-	float n = smoothstep(0.0, abs(sin(u_time * 0.05)) + 1.0, F.x) / ( abs(audio.notch));
+	vec2 F = cellular2x2x2(vec3(st * 1.0, time));
+	float n = smoothstep(0.0, abs(sin(time * 0.05)) + 1.0, F.x) / ( abs(audio.notch));
   // n = step(n, sin(pos.x));
   color = vec3(n);
-  color /= spiral_pxl(pos.yx * 5.5 * abs(audio.bandpass), wrap_time(u_time, 10.0) + 10.0);
+  color /= spiral_pxl(pos.yx * 5.5 * abs(audio.bandpass), wrap_time(time, 10.0) + 10.0);
   color.b *= 1.053 / abs(audio.bandpass);
   color.b -= 0.4;
   color.r *= 0.4;
@@ -239,6 +47,6 @@ vec3 nyc2020bb_05(vec2 pos, float u_time, peakamp audio) {
   // color = vec3(0.5, 0.5, 1.0) * color;
   color = 1.0 - color;
 
-  return color;
+  gl_FragColor = vec4(color, 1.0);
 }
 #endif
--- main.frag
+++ main.frag
@@ -24,27 +24,19 @@ uniform sampler2D u_freq_slow;
 #include "lib/common/s4y.glsl"
 #endif
 
-// #ifndef T4B_ABD_02
-// #include "lib/t420babe/abd/abd-02.glsl"
-// #endif
-
-#ifndef CELLULAR_NOISE_2_X_2_X_2
-#include "lib/cellular-noise/cellular-2x2x2.glsl"
+#ifndef T4B_ABD_01
+#include "lib/t420babe/abd/abd-01.glsl"
 #endif
 
+
 void main(void) {
-  // peakamp audio = u_audio;
-  // float audio_mul = 1.0;
-  // audio.notch *= audio_mul;
-  // audio.bandpass *= audio_mul;
-  // audio.highpass *= audio_mul;
-  // audio.lowpass *= audio_mul;
-  // // 82
-  // float time = t * 1.0 + 00.0;
-  // abd_02(p3, t, u_audio);
-  // gl_FragColor = vec4(2.0 * audio.notch, 0.3, 0.2, 1.0);
-  // gl_FragColor = vec4(sin(t), 0.4, 0.8, 1.0);
-  vec2 F = cellular2x2x2(p3.xyz);
-  float n = smoothstep(0.4, 0.5, F.x);
-  gl_FragColor = vec4(n, n, n, 1.0);
+  peakamp audio = u_audio;
+  float audio_mul = 1.0;
+  audio.notch *= audio_mul;
+  audio.bandpass *= audio_mul;
+  audio.highpass *= audio_mul;
+  audio.lowpass *= audio_mul;
+  // 82
+  float time = t * 1.0 + 00.0;
+  abd_01(p3, t, u_audio);
 }
