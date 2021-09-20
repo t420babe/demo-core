84e5aef19d4a70311891be348d0864b97cdc712c
t420babe <t420babe@gmail.com>
2021-09-20 18:36:27 -0400

    nyc2020bb -> ABE, cellular noise 2x2x2x

--- lib/t420babe/abd/abd-00.glsl
+++ lib/t420babe/abd/abd-00.glsl
@@ -1,7 +1,7 @@
 // #fuckmeup #fav5 #xtc
 // Human by Sevdaliza
-#ifndef T420BABE_NYC2020BB_00
-#define T420BABE_NYC2020BB_00
+#ifndef T4B_ABD_00
+#define T4B_ABD_00
 
 #ifndef COMMON_PEAKAMP
 #include "./lib/common/peakamp.glsl"
@@ -19,71 +19,33 @@
 #include "./lib/common/math-constants.glsl"
 #endif
 
-float center_ring(vec2 pos, float crisp) {
+#ifndef CELLULAR_NOISE_2_X_2_X_2
+#include "lib/cellular-noise/cellular-2x2x2.glsl"
+#endif
+
+float abd_00_center_ring(vec2 pos, float crisp) {
     float ring = log(dot(pos, pos));
     // Abs keeps cellular inside ring
     return abs( ring * crisp);
 }
 
-// Cellular noise, returning F1 and F2 in a vec2.
-// Speeded up by using 2x2x2 search window instead of 3x3x3,
-// at the expense of some pattern artifacts.
-// F2 is often wrong and has sharp discontinuities.
-// If you need a good F2, use the slower 3x3x3 version.
-vec2 cellular2x2x2(vec3 P) {
-	float K = 0.142857142857; // 1/7
-	float Ko = 0.428571428571; // 1/2-K/2
-	float K2 = 0.020408163265306; // 1/(7*7)
-	float Kz = 0.166666666667; // 1/6
-	float Kzo = 0.416666666667; // 1/2-1/6*2
-	float jitter = 0.8; // smaller jitter gives less errors in F2
-	vec3 Pi = mod(floor(P), 289.0);
- 	vec3 Pf = fract(P);
-	vec4 Pfx = Pf.x + vec4(0.0, -1.0, 0.0, -1.0);
-	vec4 Pfy = Pf.y + vec4(0.0, 0.0, -1.0, -1.0);
-	vec4 p = permute(Pi.x + vec4(0.0, 1.0, 0.0, 1.0));
-	p = permute(p + Pi.y + vec4(0.0, 0.0, 1.0, 1.0));
-	vec4 p1 = permute(p + Pi.z); // z+0
-	vec4 p2 = permute(p + Pi.z + vec4(1.0)); // z+1
-	vec4 ox1 = fract(p1*K) - Ko;
-	vec4 oy1 = mod(floor(p1*K), 7.0)*K - Ko;
-	vec4 oz1 = floor(p1*K2)*Kz - Kzo; // p1 < 289 guaranteed
-	vec4 ox2 = fract(p2*K) - Ko;
-	vec4 oy2 = mod(floor(p2*K), 7.0)*K - Ko;
-	vec4 oz2 = floor(p2*K2)*Kz - Kzo;
-	vec4 dx1 = Pfx + jitter*ox1;
-	vec4 dy1 = Pfy + jitter*oy1;
-	vec4 dz1 = Pf.z + jitter*oz1;
-	vec4 dx2 = Pfx + jitter*ox2;
-	vec4 dy2 = Pfy + jitter*oy2;
-	vec4 dz2 = Pf.z - 1.0 + jitter*oz2;
-	vec4 d1 = dx1 * dx1 + dy1 * dy1 + dz1 * dz1; // z+0
-	vec4 d2 = dx2 * dx2 + dy2 * dy2 + dz2 * dz2; // z+1
-
-	// Sort out the two smallest distances (F1, F2) (consider F1 only)
-	d1 = min(d1, d2);
-	d1.xy = min(d1.xy, d1.wz);
-	d1.x = min(d1.x, d1.y);
-	return sqrt(d1.xx);
-}
-
-
-vec3 nyc2020bb_00(vec2 pos, float u_time, peakamp audio) {
+void abd_00(vec3 p3, float time, peakamp audio) {
+  vec2 pos = p3.xy;
   audio.lowpass   *= 1.0;
   audio.highpass  *= 1.0;
   audio.bandpass  *= 1.0;
   audio.notch     *= 1.0;
   vec2 pos_cell = pos;
-  pos_cell *= 25.0 * abs(sin(u_time * 0.01));
+  pos_cell *= 25.0 * abs(sin(time * 0.01));
   pos_cell += 1.0;
-	vec2 F = cellular2x2x2(vec3(pos_cell, u_time));
-	float n = smoothstep(0.1, abs(sin(u_time * 0.05)) + 0.8, F.y * F.x);
+	vec2 F = cellular2x2x2(vec3(pos_cell, time));
+	float n = smoothstep(0.1, abs(sin(time * 0.05)) + 0.8, F.y * F.x);
   vec3 color = vec3(n);
 
-  color /= center_ring((pos * 3.50 * audio.notch), wrap_time(u_time, 30.0) + 10.0) * wrap_time(u_time, 10.0) / 10.0;
+  color /= abd_00_center_ring((pos * 3.50 * audio.notch), wrap_time(time, 30.0) + 10.0) * wrap_time(time, 10.0) / 10.0;
   color.b *= 1.553 - abs(audio.bandpass);
   color *= abs(audio.notch) * 5.5;
 
-  return color;
+  gl_FragColor = vec4(color, 1.0);
 }
 #endif
--- main.frag
+++ main.frag
@@ -24,19 +24,27 @@ uniform sampler2D u_freq_slow;
 #include "lib/common/s4y.glsl"
 #endif
 
-#ifndef T4B_ABE_53
-#include "lib/t420babe/abe/abe-53.glsl"
-#endif
+// #ifndef T4B_ABD_02
+// #include "lib/t420babe/abd/abd-02.glsl"
+// #endif
+
+// #ifndef CELLULAR_NOISE_2_X_2_X_2
+// #include "lib/cellular-noise/cellular-2x2x2.glsl"
+// #endif
 
 void main(void) {
-  peakamp audio = u_audio;
-  float audio_mul = 1.0;
-  audio.notch *= audio_mul;
-  audio.bandpass *= audio_mul;
-  audio.highpass *= audio_mul;
-  audio.lowpass *= audio_mul;
-  // 82
-  float time = t * 1.0 + 53.0;
-  abe_53(p3, time, audio);
+  // peakamp audio = u_audio;
+  // float audio_mul = 1.0;
+  // audio.notch *= audio_mul;
+  // audio.bandpass *= audio_mul;
+  // audio.highpass *= audio_mul;
+  // audio.lowpass *= audio_mul;
+  // // 82
+  // float time = t * 1.0 + 00.0;
+  // abd_02(p3, t, u_audio);
   // gl_FragColor = vec4(2.0 * audio.notch, 0.3, 0.2, 1.0);
+  gl_FragColor = vec4(sin(t), 0.4, 0.8, 1.0);
+  // vec2 F = cellular2x2x2(p3.xyz);
+  // float n = smoothstep(0.4, 0.5, F.x);
+  // gl_FragColor = vec4(n, n, n, 1.0);
 }
